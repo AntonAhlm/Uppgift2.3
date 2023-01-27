b60916a9d7c959123a8f4475fe0a25a01b9e99d1
Anton <>
2023-01-27 08:37:14 +0100

    Add project files.

---
+++ Uppgift2.3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Hur många dagar vill du hyra bilen? ");
+            int dagar = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Hur många kilometer kommer du köra? ");
+            int km = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Det kommer kosta " + (300+km+500*(dagar-1)) + " kr");
+
+        }
+    }
+}
